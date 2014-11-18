f3a3c1a0d6cdb85c6386997245eecc0533e941c3
McCartney Bohn <mccartneybohn@yahoo.com>
2014-11-18 12:54:39 -0700

    added has many and belongs to

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,5 +3,8 @@ class Person < ActiveRecord::Base
   def full_name
     "#{first_name} #{last_name}"
   end
-
+has_many :employments
+has_many :job_titles, through: :employments
+has_many :organizations, through: :employments
+has_many :locations, through: :employments
 end
